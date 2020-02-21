ff725fa57e121eda5f35c0277b0edd2a06602617
Juan Macias <Juan.Macias@sa.catapult.org.uk>
2020-02-21 10:07:42 +0000

    Removing unnecesary bits for locking down tab selection

--- widgets/ODCRequest/Widget.js
+++ widgets/ODCRequest/Widget.js
@@ -234,26 +234,14 @@ function(declare, BaseWidget, lang, parser, on, BorderContainer, TabContainer, C
         ]
       }, this.tabsNode);
 
-      console.log("this.tabContainer", this.tabContainer.tabItems);
-
+      // Block tab selection for Form and Submit Pane
       this.tabContainer.tabItems
         .find(tab => tab.title === 'Form Pane')
         .style.pointerEvents = 'none';
 
       this.tabContainer.tabItems
-        .find(tab => tab.title === 'Form Pane')
-        .style.cursor = "not-allowed";
-
-
-      this.tabContainer.tabItems
         .find(tab => tab.title === 'Submit Pane')
         .style.pointerEvents = 'none';
-
-      this.tabContainer.tabItems
-        .find(tab => tab.title === 'Submit Pane')
-        .style.cursor = "not-allowed";
-
-
     },
 
     onOpen: function(){
@@ -296,13 +284,10 @@ function(declare, BaseWidget, lang, parser, on, BorderContainer, TabContainer, C
       // Select the form tab panel automatically
       this.tabContainer.selectTab(this.formPaneTab.title);
 
+      // Unlock Form Pane tab selection after selecting product
       this.tabContainer.tabItems
         .find(tab => tab.title === 'Form Pane')
         .style.pointerEvents = "";
-
-      this.tabContainer.tabItems
-        .find(tab => tab.title === 'Form Pane')
-        .style.cursor = "";
     }
 
     // onClose: function(){
