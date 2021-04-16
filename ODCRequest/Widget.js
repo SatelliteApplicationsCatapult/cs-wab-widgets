06cdf24320a39d12387adbf163d0f241b2ced869
James Hinton <63542818+james-hinton@users.noreply.github.com>
2021-04-16 12:15:19 +0100

    Widget Changes (#1)
    * widget changes to allow the modification of dynamic front-end validation. Also now works with the updated version of the cubequery API endpoints.
    Co-authored-by: James Hinton <james.hinton@spatialdays.com>

--- ODCRequest/Widget.js
+++ ODCRequest/Widget.js
@@ -39,39 +39,36 @@ function(declare, BaseWidget, lang, parser, on, BorderContainer, TabContainer, C
     },
 
     startup: function() {
+
       this.inherited(arguments);
       console.log('ODCRequest::startup');
 
       this.selectPaneTab = {
-        title: "Select Pane",
+        title: "Select",
         content: this.selectPane
       };
       this.formPaneTab = {
-        title: "Form Pane",
+        title: "Options",
         content: this.formPane
       };
       this.submitPaneTab = {
-        title: "Submit Pane",
+        title: "Submit",
         content: this.submitPane
       };
 
       this.tabContainer = new TabContainer3({
         average: true,
-        tabs:[
-          this.selectPaneTab,
-          this.formPaneTab,
-          this.submitPaneTab
-        ]
+        tabs: [this.selectPaneTab, this.formPaneTab, this.submitPaneTab]
       }, this.tabsNode);
 
-      // Block tab selection for Form and Submit Pane
-      this.tabContainer.tabItems
-        .find(tab => tab.title === 'Form Pane')
-        .style.pointerEvents = 'none';
+      // Block tab selection for Form and Submit
+      this.tabContainer.tabItems.find(function (tab) {
+        return tab.title === 'Options';
+      }).style.pointerEvents = 'none';
 
-      this.tabContainer.tabItems
-        .find(tab => tab.title === 'Submit Pane')
-        .style.pointerEvents = 'none';
+      this.tabContainer.tabItems.find(function (tab) {
+        return tab.title === 'Submit';
+      }).style.pointerEvents = 'none';
 
       var newRequestButton = new Button({
         label: '↩ Request another product',
@@ -83,11 +80,12 @@ function(declare, BaseWidget, lang, parser, on, BorderContainer, TabContainer, C
       this.createSelectProductPane();
     },
 
-    onOpen: function(){
+    onOpen: function() {
       console.log('ODCRequest::onOpen');
     },
 
-    createSelectProductPane: function () {
+    createSelectProductPane: function() {
+      var _this = this;
 
       var token_url = new URL(this.config.cubequeryUrl + '/token');
 
@@ -98,66 +96,86 @@ function(declare, BaseWidget, lang, parser, on, BorderContainer, TabContainer, C
           'Content-Type': 'application/json'
         },
         body: JSON.stringify({
-          name:this.config.cubequeryName,
-          pass:this.config.cubequeryPass
+          name: this.config.cubequeryName,
+          pass: this.config.cubequeryPass
         })
-      })
-      .then((response) => {
+      }).then(function (response) {
         return response.json();
-      })
-      .then((data) => {
+      }).then(function (data) {
+          _this.apiToken = data.token;
+          var describe_url = new URL(_this.config.cubequeryUrl + '/describe');
+          describe_url.searchParams.append('APP_KEY', _this.apiToken);
+  
+          fetch(describe_url, {
+            method: 'GET',
+            mode: 'cors',
+            headers: {
+              'Content-Type': 'application/json'
+            }
+          }).then(function (response) {
+            return response.json();
+          }).then(function (data) {
+            dynamic_settings = data.settings
+            result = data.result              
+            var _iteratorNormalCompletion = true;
+            var _didIteratorError = false;
+            var _iteratorError = undefined;
 
-        this.apiToken = data.token;
-        var describe_url = new URL(this.config.cubequeryUrl + '/describe');
-        describe_url.searchParams.append('APP_KEY', this.apiToken);
+            try {
 
-        fetch(describe_url, {
-          method: 'GET',
-          mode: 'cors',
-          headers: {
-            'Content-Type': 'application/json'
-          }
-        })
-        .then((response) => {
-          return response.json();
-        })
-        .then((data) => {
+              for (var _iterator = result[Symbol.iterator](), _step; !(_iteratorNormalCompletion = (_step = _iterator.next()).done); _iteratorNormalCompletion = true) {
+                var product = _step.value;
+                var productCard = new ProductCard({
+                  name: product.name,
+                  display_name: product.display_name,
+                  description: product.description,
+                  args: product.args,
+                  formPane: _this.formPane,
+                  map: _this.map,
+                  tabContainer: _this.tabContainer,
+                  config: _this.config,
+                  apiToken: _this.apiToken,
+                  infoLink: product.info_url,
+                  thumbnail: product.img_url,
+                  dynamic_settings: dynamic_settings,
+                });
 
-          for (const product of data){
-            var productCard = new ProductCard({
-              name: product.name,
-              display_name: product.display_name,
-              description: product.description,
-              args: product.args,
-              formPane: this.formPane,
-              map: this.map,
-              tabContainer: this.tabContainer,
-              config: this.config,
-              apiToken: this.apiToken
-            });
-
-            productCard.placeAt(this.selectPane);
-            productCard.on('on-product-selected', lang.hitch(this, this._productSelectedOnChanged));
-
-            this.productCardList.push(productCard);
-          }
-        })
-      })
-      .catch((error) => {
-        this.errorTemplate = new ErrorTemplate({
-          errorMessage: error,
-          tabContainer: this.tabContainer,
+                productCard.placeAt(_this.selectPane);
+                productCard.on('on-product-selected', lang.hitch(_this, _this._productSelectedOnChanged));
+  
+                _this.productCardList.push(productCard);
+              }
+            } catch (err) {
+              _didIteratorError = true;
+              _iteratorError = err;
+            } finally {
+              try {
+                if (!_iteratorNormalCompletion && _iterator.return) {
+                  _iterator.return();
+                }
+              } finally {
+                if (_didIteratorError) {
+                  throw _iteratorError;
+                }
+              }
+            }
+          });
+        }).catch(function (error) {
+          _this.errorTemplate = new ErrorTemplate({
+            errorMessage: error,
+            tabContainer: _this.tabContainer
+          });
+  
+          _this.errorTemplate.placeAt(_this.selectPane);
         });
-
-        this.errorTemplate.placeAt(this.selectPane);
-      });
+        
     },
 
-    _productSelectedOnChanged : function(evt) {
+    _productSelectedOnChanged: function(evt) {
 
       // if any other product has been selected destroy the form and unselect it
       this.productCardList.forEach(function (productCard) {
-        if(productCard.name !== evt.name && productCard.selected){
+        if (productCard.name !== evt.name && productCard.selected) {
           productCard.requestForm.clearAoi();
           productCard.requestForm.destroy();
           productCard.unselect();
@@ -167,54 +185,54 @@ function(declare, BaseWidget, lang, parser, on, BorderContainer, TabContainer, C
       // Select the form tab panel automatically
       this.tabContainer.selectTab(this.formPaneTab.title);
 
-      // Unlock Form Pane tab selection after selecting product
-      this.tabContainer.tabItems
-        .find(tab => tab.title === 'Form Pane')
-        .style.pointerEvents = "";
+      // Unlock Options tab selection after selecting product
+      this.tabContainer.tabItems.find(function (tab) {
+        return tab.title === 'Options';
+      }).style.pointerEvents = "";
     },
 
     newRequestSubmit: function(evt) {
       // destroy the form and unselect it
       this.productCardList.forEach(function (productCard) {
-        if(productCard.selected){
+        if (productCard.selected) {
           productCard.requestForm.clearAoi();
           productCard.requestForm.destroy();
           productCard.unselect();
         }
       });
 
-      // Select the Select Pane tab automatically
+      // Select the Select tab automatically
       this.tabContainer.selectTab(this.selectPaneTab.title);
 
-      // Lock Form Pane tab
-      this.tabContainer.tabItems
-        .find(tab => tab.title === 'Form Pane')
-        .style.pointerEvents = 'none';
+      // Lock Options tab
+      this.tabContainer.tabItems.find(function (tab) {
+        return tab.title === 'Options';
+      }).style.pointerEvents = 'none';
 
-      // Unlock Select Pane tab
-      this.tabContainer.tabItems
-      .find(tab => tab.title === 'Select Pane')
-      .style.pointerEvents = '';
+      // Unlock Select tab
+      this.tabContainer.tabItems.find(function (tab) {
+        return tab.title === 'Select';
+      }).style.pointerEvents = '';
     },
 
     modifyRequestSubmit: function(evt) {
-      // Select the Form Pane tab automatically
-      this.tabContainer.selectTab('Form Pane');
+      // Select the Options tab automatically
+      this.tabContainer.selectTab('Options');
 
-      // unlock Select Pane tab
-      this.tabContainer.tabItems
-        .find(tab => tab.title === 'Select Pane')
-        .style.pointerEvents = '';
+      // unlock Select tab
+      this.tabContainer.tabItems.find(function (tab) {
+        return tab.title === 'Select';
+      }).style.pointerEvents = '';
 
-      // unlock Form Pane tab
-      this.tabContainer.tabItems
-      .find(tab => tab.title === 'Form Pane')
-      .style.pointerEvents = '';
+      // unlock Options tab
+      this.tabContainer.tabItems.find(function (tab) {
+        return tab.title === 'Options';
+      }).style.pointerEvents = '';
 
-      // Lock Submit Pane tab
-      this.tabContainer.tabItems
-        .find(tab => tab.title === 'Submit Pane')
-        .style.pointerEvents = 'none';
+      // Lock Submit tab
+      this.tabContainer.tabItems.find(function (tab) {
+        return tab.title === 'Submit';
+      }).style.pointerEvents = 'none';
     }
 
     // onClose: function(){
